903b6485901ffd27b570e782749dd0e3784401d1
fofinhaCODE <00001132754781sp@al.educacao.sp.gov.br>
2026-03-17 11:30:18 -0300

    Add files via upload

---
+++ .py
@@ -0,0 +1,7 @@
+media1 = float(input("Digite sua nota: "))
+media2 = float(input("Digite sua nota: "))
+media3 = float(input("Digite sua nota: "))
+media4 = float(input("Digite sua nota: "))
+
+resultado = ( media1 + media2 + media3 + media4)
+print ("Resultado final: ", resultado)
